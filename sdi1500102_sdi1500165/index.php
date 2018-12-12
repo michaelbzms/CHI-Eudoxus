ac643e33ccaf28a96d822a80d9adb9edaba80ec9
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2018-12-12 15:27:38 +0200

    Home Page Basic Layout
    I couldn't get the text to align vertically at the bottom instead of the top. Any ideas?

--- sdi1500102_sdi1500165/index.php
+++ sdi1500102_sdi1500165/index.php
@@ -1,41 +1,52 @@
 <!DOCTYPE html>
+<?php $active_page = "HomePage"; ?>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>Eudoxus</title>
+    <!-- CSS -->
     <link rel="stylesheet" type="text/css" href="/sdi1500102_sdi1500165/css/index.css"/>
+    <link rel="stylesheet" type="text/css" href="/sdi1500102_sdi1500165/css/navbar.css"/>
+	<link rel="stylesheet" type="text/css" href="/sdi1500102_sdi1500165/css/lib/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="/sdi1500102_sdi1500165/css/lib/bootstrap-grid.min.css"/>
+	<!-- JS -->
+	<script src="/sdi1500102_sdi1500165/javascript/lib/jquery-3.3.1.min.js"></script>
+	<script src="/sdi1500102_sdi1500165/javascript/lib/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="main-container">
         <a href="/sdi1500102_sdi1500165/index.php"><img id="logo" src="/sdi1500102_sdi1500165/images/logo.png"/></a>
-        <h1 id="title">Ηλεκτρονική Υπηρεσία <br>Ολοκληρωμένης Διαχείρισης <br>Συγγραμμάτων και Λοιπών Βοηθημάτων</h1>
+        <h1 id="title">Ηλεκτρονική Υπηρεσία<br>Ολοκληρωμένης Διαχείρισης<br>Συγγραμμάτων και Λοιπών Βοηθημάτων</h1>
         <img id="slogan" src="/sdi1500102_sdi1500165/images/slogan.png"/>
         <!-- Global search bar -->
-        <img id="global_help" src="/sdi1500102_sdi1500165/images/help_icon.png"/>
+        <img id="global_help" src="/sdi1500102_sdi1500165/images/help_icon.png"/><!-- add onClick() event -->
         <form id="global_search_form" action="#" method="get">
-            <input type="text" name="global_search_str">
+            <input class="form-control" type="text" name="global_search_str" placeholder="Αναζήτηση">
             <!-- submit with 'enter' -->
         </form>
-        <!-- add onClick() event -->
         <!-- common navbar for all without login -->
-        <nav class="navbar-general">
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/index.php">Αρχική Σελίδα</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/announcements.php">Ανακοινώσεις</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/booksearch.php">Αναζήτηση Συγγραμμάτων</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/selectedbooks.php">Επιλεγμένα Συγγράμματα</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/faq.php">FAQ</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/communication.php">Επικοινωνία</a></div>
-            <div class="navbar-general-item"><a href="/sdi1500102_sdi1500165/php/aboutus.php">About us</a></div>
-        </nav>
+        <?php include("php/general_navbar.php"); ?>
         <!-- specific navbar for each category -->
-        <nav class="navbar-specific">
-            <div class="navbar-specific-item">Είμαι... ►<div>
-            <div class="navbar-specific-item"><button>Φοιτητής</button></div>
-            <div class="navbar-specific-item"><button>Εκδότης</button></div>
-            <div class="navbar-specific-item"><button>Υπάλληλος Γραμματείας Τμήματος</button></div>
-            <div class="navbar-specific-item"><button>Υπάλληλος Σημείου Διανομής</button></div>
-            <div class="navbar-specific-item"><button>Άλλο</button></div>
-        </nav>
+        <div class="specific_navbar">
+            <div class="navbar-specific-item">
+                <div id="iam">Είμαι... ►</div>
+            </div>
+            <div class="navbar-specific-item">
+                <div id="student">Φοιτητής</div>
+            </div>
+            <div class="navbar-specific-item">
+                <div id="publisher">Εκδότης</div>
+            </div>
+            <div class="navbar-specific-item">
+                <div id="secretary">Υπάλληλος<br>Γραμματείας<br>Τμήματος</div>
+            </div>
+            <div class="navbar-specific-item">
+                <div id="distribution_point">Υπάλληλος<br>Σημείου<br>Διανομής</div>
+            </div>
+            <div class="navbar-specific-item">
+                <div id="other">Άλλο</div>
+            </div>
+        </div>
         <?php include "footer.html"; ?>
     </div>
 </body>
---
+++ sdi1500102_sdi1500165/php/general_navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-dark general_navbar">
+    <ul class="nav"  role="navigation">
+        <!-- Fact: &nbsp is a space that will not break to a new line -->
+        <li class="nav-item">
+            <a class="nav-link <?php if ( $active_page == "HomePage" ) { print "active"; } ?>" href="/sdi1500102_sdi1500165/index.php">Αρχική&nbspΣελίδα</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/php/announcements.php">Ανακοινώσεις</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/php/book_search.php">Αναζήτηση&nbspΣυγγραμμάτων</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/php/selected_books.php">Επιλεγμένα&nbspΣυγγράμματα</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/faq.html">FAQ</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/php/communication.php">Επικοινωνία</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/sdi1500102_sdi1500165/php/aboutus.php">About&nbspus</a>
+        </li>
+    </ul>
+</nav>
